3ce46cbd48827a1c13de1f911c43ffaa5bd9f76e
Pratik Solanki <45298146+pratikksolanki@users.noreply.github.com>
2021-01-14 22:14:05 -0500

    Initial Commit

---
+++ app.js
@@ -0,0 +1,20 @@
+//Countdown Function
+var countDownDate = new Date("Jan 30, 2021 23:59:59").getTime();
+var x = setInterval(function() {
+var now = new Date().getTime();
+var timeRemaining = countDownDate - now;
+var days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
+var hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+var minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
+var seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
+
+document.getElementById("launch").innerHTML = days + "d " + hours + "h "
++ minutes + "m " + seconds + "s";
+
+if (timeRemaining < 0) {
+    clearInterval(x);
+    document.getElementById("launch").innerHTML = "EXPIRED";
+}
+}, 1000);
+
+//Auto Year Change Function
