90767b2547841d2699b237369513a739ae771bc6
Pratik Solanki <45298146+pratikksolanki@users.noreply.github.com>
2021-02-05 03:10:45 -0500

    countdown timer bug

--- app.js
+++ app.js
@@ -8,12 +8,12 @@ var hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60
 var minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
 var seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
 
-document.getElementById("launch").innerHTML = days + "d " + hours + "h "
+document.getElementById("launch").innerHTML = "in " + days + "d " + hours + "h "
 + minutes + "m " + seconds + "s";
 
 if (timeRemaining < 0) {
     clearInterval(x);
-    document.getElementById("launch").innerHTML = "EXPIRED";
+    document.getElementById("launch").innerHTML = "soon";
 }
 }, 1000);
 
