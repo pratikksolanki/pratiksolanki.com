9957d94fc9d49e8ecdc255ce9f5488ad7a685709
Pratik Solanki <45298146+pratikksolanki@users.noreply.github.com>
2021-04-04 20:30:23 -0400

    unnecessary js removed

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 //Countdown Function
-var countDownDate = new Date("Feb 28, 2021 23:59:59").getTime();
-var x = setInterval(function() {
-var now = new Date().getTime();
-var timeRemaining = countDownDate - now;
-var days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
-var hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-var minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
-var seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
+// var countDownDate = new Date("Feb 28, 2021 23:59:59").getTime();
+// var x = setInterval(function() {
+// var now = new Date().getTime();
+// var timeRemaining = countDownDate - now;
+// var days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
+// var hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+// var minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
+// var seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
 
-document.getElementById("launch").innerHTML = "in " + days + "d " + hours + "h "
-+ minutes + "m " + seconds + "s";
+// document.getElementById("launch").innerHTML = "in " + days + "d " + hours + "h "
+// + minutes + "m " + seconds + "s";
 
-if (timeRemaining < 0) {
-    clearInterval(x);
-    document.getElementById("launch").innerHTML = "soon";
-}
-}, 1000);
+// if (timeRemaining < 0) {
+//     clearInterval(x);
+//     document.getElementById("launch").innerHTML = "soon";
+// }
+// }, 1000);
