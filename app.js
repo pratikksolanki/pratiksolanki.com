394fa3b1a91f12a87a9291919bb615a7c322ad7e
Pratik Solanki <45298146+pratikksolanki@users.noreply.github.com>
2022-01-07 22:22:30 -0500

    UI update

--- app.js
+++ app.js
@@ -1,18 +1,37 @@
-//Countdown Function
-// var countDownDate = new Date("Feb 28, 2021 23:59:59").getTime();
-// var x = setInterval(function() {
-// var now = new Date().getTime();
-// var timeRemaining = countDownDate - now;
-// var days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
-// var hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-// var minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
-// var seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
+// Nav Bar
+const tabButtons = Array.from(document.getElementsByClassName('tab-btn'));
+let currentTab = tabButtons[0];
 
-// document.getElementById("launch").innerHTML = "in " + days + "d " + hours + "h "
-// + minutes + "m " + seconds + "s";
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll("header nav ul li");
 
-// if (timeRemaining < 0) {
-//     clearInterval(x);
-//     document.getElementById("launch").innerHTML = "soon";
-// }
-// }, 1000);
+const tabHighlight = document.getElementById('tab-btn-highlight');
+
+const activeToggle = (item) => {
+    currentTab.classList.remove('tab-btn-highlight');
+    currentTab = item;
+    item.classList.add('tab-btn-highlight');
+}
+
+tabButtons.forEach(item => {  
+    item.addEventListener('click', () => activeToggle(item));
+});
+
+window.addEventListener('scroll', () => {
+    let current = '';
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.clientHeight;
+        if (scrollY >= (sectionTop - sectionHeight/3)){
+            current = section.getAttribute('id');
+        }
+    })
+
+    navLinks.forEach( li => {
+        li.classList.remove('tab-btn-highlight');
+        if (li.classList.contains(current)){
+            li.classList.add('tab-btn-highlight')
+        }
+    })
+})
+
