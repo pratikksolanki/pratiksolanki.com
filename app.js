fdaad8a04915d6cdad3e14a1c50da6ce15e17480
Pratik Solanki <45298146+pratikksolanki@users.noreply.github.com>
2022-01-10 19:30:14 -0500

    changed navbar display behaviour on scroll

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
-// Nav Bar
+
+let prevPos = window.pageYOffset;
 const tabButtons = Array.from(document.getElementsByClassName('tab-btn'));
 let currentTab = tabButtons[0];
-
 const sections = document.querySelectorAll("section");
 const navLinks = document.querySelectorAll("header nav ul li");
-
 const tabHighlight = document.getElementById('tab-btn-highlight');
+
+window.onscroll = function() {
+    let curPos = window.pageYOffset;
+    if (prevPos > curPos) {
+      document.getElementById("nav").style.top = "0";
+    } else {
+      document.getElementById("nav").style.top = "-100px";
+    }
+    prevPos = curPos;
+  }
 
 const activeToggle = (item) => {
     currentTab.classList.remove('tab-btn-highlight');
@@ -34,4 +43,6 @@ window.addEventListener('scroll', () => {
         }
     })
 })
+
+
 
